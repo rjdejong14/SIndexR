795925d2702618601f5c40a60a89d050b239a7a4
Yong Luo <yluo1@lakeheadu.ca>
2018-01-26 11:37:08 -0800

    [SIndexR_VersionNumber] improved

---
+++ R/SIndexR_VersionNumber.R
@@ -0,0 +1,33 @@
+#' @title
+#' Sindex version
+#' @description
+#' Returns the version number of the Sindex routines.
+#'
+#' @return
+#' The number indicating the version of the Sindex routines.
+#'
+#' @note
+#' The format of the number is always in the form of Mmm:
+#'    where  M:    the major release number (1, 2, ...)
+#'          mm:    the minor release number (0, 1, ..., 99)
+#'
+#'    An example would be: 631, meaning version 6.31
+#'
+#'    If the major release is greater than what your application expects,
+#'    assume that the Sindex routines cannot be used, and that the user
+#'    needs to obtain a newer version of sindex.dll.
+#'
+#'    Minor release changes will include the following:
+#'     - addition of a function
+#'     - changed return values (i.e. error messages)
+#'     - iterating for solutions may generate different results
+#'     - bug fixes in implementation of site index equations
+#'     - addition of species
+#'     - addition of curve sources (equations)
+#'     - change of default curve for a species
+#'     - change of mapping species to a different species
+#' @export
+#' @rdname SIndexR_VersionNumber
+SIndexR_VersionNumber <- function(){
+  return(Sindex_VersionNumber())
+}
