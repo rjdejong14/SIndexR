4b638803f578e7c462dcfef86285ec3ed26e4b1a
Yong Luo <yluo1@lakeheadu.ca>
2018-01-26 17:04:42 -0800

    [SIndexR_SpecMap] function name changed and allow multiple entries

--- R/SpeciesMapRemap.R
+++ R/SpeciesMapRemap.R
@@ -1,24 +1,23 @@
-  #' @title
+#' @title
 #'    Determine species index from species code
 #' @description
 #'    Determine species index from species code
 #' @param sc Character, Defined as species code.
 #' @return Species index.
 #'    May return an error code under the following conditions:
-#'    
+#'
 #'    return value    condition
 #'    ------------    ---------
-  #'    SI_ERR_CODE     if species code is unknown
+#'    SI_ERR_CODE     if species code is unknown
 #'
 #' @note
 #'    Species code string can be 1, 2, or 3 letters; upper/lower case
 #'      is ignored.
-#' @rdname Sindex_SpecMap
-Sindex_SpecMap <- function(sc)
+#' @rdname SIndexR_SpecMap
+SIndexR_SpecMap <- function(sc)
 {
-  return(species_map (sc))
+  return(unlist(lapply(sc, function(s) species_map (s))))
 }
-
 
 
 #' @title
@@ -31,19 +30,34 @@ Sindex_SpecMap <- function(sc)
 #'
 #'  @return Species index.
 #'    May return an error code under the following conditions:
-#'    
+#'
 #'    return value    condition
 #'    ------------    ---------
-  #'    SI_ERR_CODE     if species code is unknown
+#'    SI_ERR_CODE     if species code is unknown
 #'    SI_ERR_FIZ      if FIZ code is unknown
 #'
-#' @note 
+#' @note
 #'    Species code string can be 1, 2, or 3 letters; upper/lower case
 #'      is ignored.  FIZ is only used where needed, such as for species
 #'      code "FD".
-#' @rdname   Sindex_SpecRemap
-Sindex_SpecRemap <- function(sc, fiz)
+#' @rdname   SIndexR_SpecRemap
+SIndexR_SpecRemap <- function(sc, fiz)
 {
+  if(length(sp_index) == 1 & length(cu_index) != 1){
+    sp_index <- rep(sp_index, length(cu_index))
+  }
+  if(length(sp_index) != 1 & length(cu_index) == 1){
+    cu_index <- rep(cu_index, length(sp_index))
+  }
+  if(length(sp_index) != length(cu_index)){
+    stop("sp_index and cu_index do not have same length.")
+  }
+  sp_index_list <- lapply(sp_index, function(s) s)
+  cu_index_list <- lapply(cu_index, function(s) s)
+  allinputs <- Map(list, sp_index_list, cu_index_list)
+  return(unlist(lapply(allinputs, function(s) Sindex_NextCurve(sp_index = s[[1]],
+                                                               cu_index = s[[2]]))))
+
   return(species_remap (sc, fiz))
 }
 
