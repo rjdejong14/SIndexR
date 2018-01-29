bbbfd694b6ddcb78060efa1c719727487c290c0d
Yong Luo <yluo1@lakeheadu.ca>
2018-01-29 11:01:39 -0800

    [SIndexR_Y2BH05] improved for name and inputs

---
+++ R/SIndexR_Y2BH05.R
@@ -0,0 +1,37 @@
+#' @title
+#'     Calculates the number of years a stand takes to grow from seed to
+#'     breast height.
+#' @description
+#'     Calculates the number of years a stand takes to grow from seed to
+#'     breast height, in steps ending in 0.5 (i.e. 0.5, 1.5. 2.5, etc.)
+#' @param curve Integer, The particular site index curve to project the height and age along.
+#' @param siteIndex Numeric, The site index value of the stand.
+#' @return \code{output} contains calculated year to breast height
+#'         \code{error} contains error information, i.e.,
+#'         0, or an error code under the following conditions:
+#'
+#'         return value    condition
+#'         ------------    ---------
+#'         SI_ERR_CURVE    input curve is not a valid curve index.
+#'         SI_ERR_GI_TOT   if GI curve
+#'         SI_ERR_LT13     if site index <= 1.3
+#' @importFrom data.table data.table
+#' @rdname SIndexR_Y2BH05
+SIndexR_Y2BH05 <- function(curve,
+                           siteIndex){
+
+  curve <- wholeToInteger(curve, "curve")
+  inputdata <- data.table::data.table(curve, siteIndex)
+  rm(curve, siteIndex)
+  curve_list <- as.list(inputdata$curve)
+  siteIndex_list <- as.list(inputdata$siteIndex)
+  inputdata_list <- Map(list, curve_list, siteIndex_list)
+
+  y2bh <- unlist(lapply(inputdata_list,
+                        function(s) si_y2bh05(cu_index = s[[1]],
+                                              site_index = s[[2]])))
+  error <- y2bh
+  error[error > 0] <- 0
+  return(list(output = y2bh,
+              error = error))
+}
