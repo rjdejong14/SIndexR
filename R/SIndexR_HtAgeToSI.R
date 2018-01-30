7094f0ff9e38f9c642d5c5b08bef3625b7345f03
Yong Luo <yluo1@lakeheadu.ca>
2018-01-30 15:16:29 -0800

    added license to each of file

--- R/SIndexR_HtAgeToSI.R
+++ R/SIndexR_HtAgeToSI.R
@@ -1,3 +1,15 @@
+# Copyright 2018 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
 #' @title
 #' Calculates site index based on height, age and site index curve.
 #' @description Converts a Height and Age to a Site Index for a particular Site Index
